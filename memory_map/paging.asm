4aeac342beaa07b4e690b7db0bfbc46e66a96a16
ilya101010 <ilya101010@gmail.com>
2017-02-04 18:32:20 +0300

    Paging! (again)

--- memory_map/paging.asm
+++ memory_map/paging.asm
@@ -6,7 +6,7 @@ include 'procedures.inc'
 section '.text' executable
 use32
 
-public init_paging
+public enable_paging
 
 pd_add = 0xB000
 pd_size = 4
@@ -15,66 +15,13 @@ pd_num = 1024
 ; [pde, pde+0x1000] - PDE 1
 ; [pde+0x1000+1+(i-1)]
 
-macro set_entry dst, src, flags
-{
-	mov eax, src
-	and eax, 0xFFFFF000
-	or eax, flags
-	mov [dst], eax
-}
-
-init_paging:
-	mbp
-.clean:
-	Fill_zeros PD, 0x1000
-	Fill_zeros PT, 2*0x1000
-.pd_set:
-	mov edi, PD
-	mov ecx, 1024
-	.lp1:
-		set_entry edi, PT, 000000000011b
-		add edi, 4
-	loop .lp1
-.pe_set:
-	mov edi, PT
-	mov ecx, 1024
-	mbp
-	.lp2:
-		mov eax, 0x8000
-		and eax, 0xFFFFF000 ; align
-		or eax, 000000000011b
-		stosd
-	loop .lp2
-	mov edi, PT+4*0xB8
-	mov eax, 0xB8000
-	and eax, 0xFFFFF000 ; align
-	or eax, 000000000011b
-	stosd
-	mbp
-.enable:
-	mov eax, PD
+enable_paging:;(void * PD) ; ret, cause low memory mapped onto itself
+	push ebp
+	mov ebp, esp
+	mov eax, [ebp+8]
 	mov cr3, eax
 	mov eax, cr0
 	or eax, 0x80000000
 	mov cr0, eax
-
-	mbp
-
-	Print paging, 2, 0x0a
-
-	jmp $
-
-paging: db "paging!",0
-
-set_address: ; esi - address of entry, eax - first 12 bits - flags
-
-setup_table: ; eax - # of table in PD, bh - flags, edx - address
-.pd_entry_setup:
-	
-	ret	
-
-setup:
-	Fill_zeros PD, 0x1000
-
-PD = pd_add
-PT = pd_add+pd_num*pd_size
+	pop ebp
+	ret
--- memory_map/paging.c
+++ memory_map/paging.c
@@ -4,9 +4,25 @@
 
 #define p_entry(addr, f) (addr << 12) | f
 
+#define DEBUG
+#define mbp
+#ifdef DEBUG
+	#define mbp asm("xchgw %bx, %bx")
+#endif
+
+#define PD PD_a
+#define PDE(i) PD+4*i
+#define PT(i) PD+0x1000+0x1000*i
+#define PTE(n, i) PT(n)+4*i
+
+#define pg_P 1
+#define pg_R 2
+#define pg_U 4
+
 extern void print(char* src, int y, uint8_t color);
 extern void hex_f(int n, char *s);
 extern void fill_zeros(void* dst, size_t size);
+extern void enable_paging(void* dst);
 
 typedef struct mmap_entry
 {
@@ -19,6 +35,12 @@ typedef struct mmap_entry
 
 typedef enum stackop {PUSH,POP,GET,SET,SIZE} stackop;
 
+void* PD_a;
+
+// Happy the man, and happy he alone
+// He who can call today his own,
+// He who, secure within, can say:
+// Tomorrow do thy worst, for I have lived today
 mmap_entry* stack(stackop operation, mmap_entry* arg)
 {
 	static mmap_entry* list[20] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
@@ -31,13 +53,13 @@ mmap_entry* stack(stackop operation, mmap_entry* arg)
 			i++;
 			return arg;
 		case POP:
-			if(i == 0) return 0;
+			if(i == 0) return list[0];
+			i--;
 			tmp = list[i];
 			list[i] = 0;
-			i--;
-			return 0xa000;
+			return tmp;
 		case GET:
-			return list[i];
+			return list[(int)arg];
 		case SET:
 			list[i] = arg;
 		case SIZE:
@@ -66,27 +88,103 @@ void demo()
 	volatile mmap_entry* region = (volatile mmap_entry*)(0xA000);
 	int i = 0;
 	int R = 0;
+	
 	while(region->type != 0)
 	{
-		if(region -> type == 1)
+		if(region->type == 1)
 			stack(PUSH,region);
+		*region++;
+	}
+
+	for(i = 0; i < stack(SIZE,0); i++)
+	{
+		region = stack(GET,i);
 		hex_f(region->type,strs[1]+1);
 		hex_f(region->base_high,strs[1]+10);
 		hex_f(region->base_low,strs[1]+18);
 		hex_f(region->length_high,strs[1]+27);
 		hex_f(region->length_low,strs[1]+35);
-		if(region->type == 1) hex_f((region->length_low)/0x1000,strs[1]+45), R += (region->length_low)/0x1000; // 32 bit!
-		else hex_f(0,strs[1]+45);
 		print(strs[1],i+3,0x0F);
-		*region++; // is actually +20 bytes (!)
-		i++;
 	}
+
+	paging_init();
+
 	print(strs[3],i+3,0x0F);
 	hex_f(stack(SIZE,0),strs[4]+24);
-	print(strs[4],i+4,0x0F);
+	print(strs[4],10,0x0F);
+
+	while(1);
 }
 
 void paging_init()
 {
-	fill_zeros(0xA000,1024*0x1000);
+	mmap_entry region;
+	char* bingo = "BINGO! 00000000";
+	int length, dlt;
+	for(int i = 1; i < stack(SIZE,0); i++) // not the first region - generally used by bootloader, GDT, IDT - no need for mess in there
+	{
+		region = *(stack(GET,i));
+		/*
+		dlt = 0;
+		if((region.base_low & 0xfff) != 0) region.base_low += 0x1000;
+		dlt = region.base_low - (region.base_low & 0xfffff000);
+		region.base_low &= 0xfffff000;
+		region.length_low -= dlt;
+		length = region.length_low;*/
+		if(region.length_low > 0x1000+0x1000*1024)
+		{
+			hex_f(region.length_low,bingo+7);
+			print(bingo,20,0x0f);
+			PD = region.base_low; // I hope VERY much, that it takes place in low 32 bits...
+			break; // now, region
+		}
+	}
+
+	char* hurray = "PAGING!               ";
+	hex_f(PD,hurray+8);
+	print(hurray,17, 0xF);
+
+	// first - PD
+	fill_zeros(PD,0x1000+0x1000*1024);
+	int pt = 0, pte = 0;
+	// page dir
+	volatile int* e = (volatile int*)PD;
+	for(int i = 0; i<1024; i++)
+	{
+		*e = PD+0x1000*(i+1);
+		*e |= pg_P | pg_U;
+		*e++;
+	}
+	// page tables
+
+	int R = 0;
+	region = *stack(GET,R);
+
+	/*
+	int reg_in = region.base_low & 0xfffff000 + (((region.base_low & 0xfffff000) != region.base_low) ? 0x1000 : 0);
+	reg_in |= pg_P | pg_U;
+	hex_f(region.length_low,bingo+7);
+	print(bingo,10+R,0x0f);
+	*/
+	for(int i = 0; i<1024*1024; i++)
+	{		
+		//region.length_low -= 0x1000;
+		//reg_in |= pg_P | pg_U;
+		*e = 0x1000*i;
+		*e |= pg_P | pg_U | pg_R;
+		/*
+		if(region.length_low == 0 && (R+1) < stack(SIZE,0))
+		{
+			R++;
+			region = *stack(GET,R);
+			
+			hex_f(region.length_low,bingo+7);
+			print(bingo,10+R,0x0f);
+		}*/
+		*e++;
+	}
+
+	mbp;
+	enable_paging(PD);
+
 }
