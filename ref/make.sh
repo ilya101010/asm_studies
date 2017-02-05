12cead4f401fe89585295d5ad019fcd77b97ce13
ilya101010 <ilya101010@gmail.com>
2017-02-05 13:15:15 +0300

    Reference init

---
+++ ref/make.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo ">>> fasm"
+fasm boot.asm boot.o
+fasm procedures.asm procedures.o
+fasm paging.asm paging.o
+gcc -m32 -o0 -c paging.c -o paging.c.o -ffreestanding -nostdlib -lgcc
+echo ">>> linking"
+ld -T linker.ld -melf_i386
+wc -c final.img
