5287211de63d6ca876b6df37c0c4e5c8bd6c6109
ilya101010 <ilya101010@gmail.com>
2017-02-27 16:49:27 +0300

    Sync: working on DAP, fixed linking & make.sh

--- ref/make.sh
+++ ref/make.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 echo ">>> init"
 mkdir -p obj
-rm obj/*
+rm obj/* -rf
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color
+printf "${CYAN}>> BOOT & INIT <<${NC}\n"
 echo ">>> fasm"
 fasm src/boot.asm obj/boot.o
-fasm src/procedures.asm obj/procedures.kernel.o
-fasm src/paging.asm obj/paging.kernel.o
-fasm src/msr.asm obj/msr.kernel.o
-fasm src/sys_routine.asm obj/sys_routine.kernel.o
+fasm src/procedures.asm obj/procedures.o
+fasm src/paging.asm obj/paging.o
+fasm src/msr.asm obj/msr.o
 echo ">>> gcc"
-gcc -m32 -o0 -c src/kernel.c -o obj/main.kernel.o -Iinclude -ffreestanding -nostdlib -lgcc -w
-gcc -m32 -o0 -c src/memory.c -o obj/memory.kernel.o -Iinclude -ffreestanding -nostdlib -lgcc -w
-gcc -m32 -o0 -c src/memtab.c -o obj/memtab.kernel.o -Iinclude -ffreestanding -nostdlib -lgcc -w
-gcc -m32 -o0 -c src/tty.c -o obj/tty.kernel.o -Iinclude -ffreestanding -nostdlib -lgcc -w
-gcc -m32 -o0 -c src/stack.c -o obj/stack.kernel.o -Iinclude -ffreestanding -nostdlib -lgcc -w
-gcc -m32 -o0 -c src/paging.c -o obj/paging.c.kernel.o -Iinclude -ffreestanding -nostdlib -lgcc -w
-gcc -m32 -o0 -c src/gdt.c -o obj/gdt.kernel.o -Iinclude -ffreestanding -nostdlib -lgcc -w
-gcc -m32 -o0 -c src/tss.c -o obj/tss.kernel.o -Iinclude -ffreestanding -nostdlib -lgcc -w
+gcc -m32 -o0 -c src/kernel.c -o obj/main.o -Iinclude -ffreestanding -nostdlib -lgcc -w
+gcc -m32 -o0 -c src/memory.c -o obj/memory.o -Iinclude -ffreestanding -nostdlib -lgcc -w
+gcc -m32 -o0 -c src/memtab.c -o obj/memtab.o -Iinclude -ffreestanding -nostdlib -lgcc -w
+gcc -m32 -o0 -c src/tty.c -o obj/tty.o -Iinclude -ffreestanding -nostdlib -lgcc -w
+gcc -m32 -o0 -c src/stack.c -o obj/stack.o -Iinclude -ffreestanding -nostdlib -lgcc -w
+gcc -m32 -o0 -c src/paging.c -o obj/paging.c.o -Iinclude -ffreestanding -nostdlib -lgcc -w
+gcc -m32 -o0 -c src/gdt.c -o obj/gdt.o -Iinclude -ffreestanding -nostdlib -lgcc -w
+gcc -m32 -o0 -c src/tss.c -o obj/tss.o -Iinclude -ffreestanding -nostdlib -lgcc -w
 echo ">>> linking boot & init"
-$(cp linker.ld obj; cd obj; ld -T linker.ld -melf_i386)
+$(cp linker.ld obj; cd obj; ld -T linker.ld -melf_i386 *.o)
 if [ -f obj/final.img ]; then
 	mv obj/final.img .
 fi
-echo ">>> final.img size"
+printf "${CYAN}>> SYS_ROUTINE <<${NC}\n"
+mkdir obj/sysr
+fasm src/sys_routine.asm obj/sysr/sys_routine.o
+
+echo ">>> boot.img size"
 wc -c final.img
